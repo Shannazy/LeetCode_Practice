4da72753257cdf51a27749e69b66df3879efa087
Shazidul Islam <shaziislam417@gmail.com>
2022-04-22 21:16:17 -0400

    V1 solution with a try catch

---
+++ Palindrome.java
@@ -0,0 +1,25 @@
+class Solution {
+  public boolean isPalindrome(int x){
+    if (x < 0){
+      return false;
+    }
+    else {
+      String temp = Integer.toString(x);
+      String reverse = "";
+      char myChar;
+
+      for(int i = 0; i<temp.length(); i++){
+        myChar = temp.charAt(i);
+        reverse = myChar+reverse;
+      }
+      try{
+      if (x == Integer.valueOf(reverse)){
+        return true;
+      }
+      else{return false;}
+    } catch (Exception e){
+      return false;
+    }
+    }
+  }
+}
